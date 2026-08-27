643587416e78c9ba9adf695264a84f2889b3fdb7
Zzzero980127 <skylgds@gmail.com>
2026-08-27 19:23:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -337,6 +337,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hint))
 
 if __name__ == "__main__":
+    
 from test import test_sync
 
 @app.route('/test')
